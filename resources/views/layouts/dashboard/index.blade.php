ab9796555718878fb7bbce4af426bd48a2cd7633
Jean <poolgamer0920u@gmail.com>
2022-05-30 20:27:28 -0400

    solucionado error de pagina de inicio

--- resources/views/layouts/dashboard/index.blade.php
+++ resources/views/layouts/dashboard/index.blade.php
@@ -33,7 +33,7 @@
         <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background: #1D3354">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="auth">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/auth">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
